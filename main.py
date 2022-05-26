d920c5f9b86323ff0b05c9524a80d70b3c547add
Sujan Koirala <077bct087.sujan@pcamupus.edu.np>
2022-05-26 21:00:40 +0545

    Button operation success

--- main.py
+++ main.py
@@ -3,19 +3,53 @@ from tkinter import *
 root = Tk()
 root.title("Tic-Tac-Toe")
 root.geometry('440x390')
+state = 0
+buttonList = []
+#   Make window unresizable
+root.resizable(False, False)
 
-def createButton(rowPos, columnPos):
-    return Button(root, text="", height = 7, width =18).grid(row = rowPos, column = columnPos, padx =5, pady =5)
+def buttonClick(buttonVal):
+    global state
+    state += 1
+    if (state % 2 == 0):
+        buttonList[buttonVal-1].config(text = "O")
+    else:
+        buttonList[buttonVal-1].config(text = "X")
+        
+    print(state)
+    # pass
+
+
+def createButton( flag):
+    return Button(root, text="", height = 7, width =18, command=lambda:buttonClick(flag))
+
 
 #    Nine buttons
-b1 = createButton(0, 0)
-b2 = createButton(0, 1)
-b3 = createButton(0, 2)
-b4 = createButton(1, 0)
-b5 = createButton(1, 1)
-b6 = createButton(1, 2)
-b7 = createButton(2, 0)
-b8 = createButton(2, 1)
-b9 = createButton(2, 2)
+b1 = createButton(1)  # Parameters -> flag
+b2 = createButton(2)
+b3 = createButton(3)
+b4 = createButton(4)
+b5 = createButton(5)
+b6 = createButton(6)
+b7 = createButton(7)
+b8 = createButton(8)
+b9 = createButton(9)
+
+buttonList += [b1, b2, b3, b4, b5, b6, b7, b8, b9]
+
+b1.grid(row = 0, column = 0, padx =5, pady =5)
+b2.grid(row = 0, column = 1, padx =5, pady =5)
+b3.grid(row = 0, column = 2, padx =5, pady =5)
+b4.grid(row = 1, column = 0, padx =5, pady =5)
+b5.grid(row = 1, column = 1, padx =5, pady =5)
+b6.grid(row = 1, column = 2, padx =5, pady =5)
+b7.grid(row = 2, column = 0, padx =5, pady =5)
+b8.grid(row = 2, column = 1, padx =5, pady =5)
+b9.grid(row = 2, column = 2, padx =5, pady =5)
+
+
+
+
+#   Adjust Buttons to list
 
 root.mainloop()
